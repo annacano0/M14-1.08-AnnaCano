36ffeed44e36bd225e8dcf93c9c453b8383289d2
annacano0 <anna.elisabet5@gmail.com>
2024-10-24 18:31:06 +0200

    add: project mid-way

---
+++ src/components/Message.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  logedUser: String,
+})
+
+let user = props.logedUser
+let newMessage = ref('')
+
+const emit = defineEmits(['postMessage'])
+const managePostMessage = () => {
+  console.log(user + ' ' + newMessage.value)
+
+  emit('postMessage', { user, newMessage })
+  newMessage.value = ''
+}
+</script>
+
+<template>
+  <input
+    v-model="newMessage"
+    type="text"
+    placeholder="Escriu el teu missatge"
+  />
+  <button @click="managePostMessage">Envia</button>
+</template>
+
+<style scoped></style>
---
+++ src/components/Xat.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import Message from './Message.vue'
+let messages = ref([])
+
+const logedUser = 'Anna'
+
+const postMessage = newMessage => {
+  console.log('posting message')
+  messages.value.push(newMessage)
+}
+</script>
+
+<template>
+  <h1>Xat entre usuaris</h1>
+  <ul class="message-container">
+    <li v-for="(message, index) in messages" :key="index">
+      {{ message.user }}{{ message.text }}
+    </li>
+  </ul>
+  <Message :logedUser="logedUser" :post-message="postMessage" />
+</template>
+
+<style scoped></style>
